351b5e5ea10634471d587806c06cd72f96220fd1
soklinNuon1 <soklin061@gmail.com>
2025-08-07 14:39:49 +0700

    success book screen

--- resources/js/Pages/WebPages/Books.jsx
+++ resources/js/Pages/WebPages/Books.jsx
@@ -1,4 +1,4 @@
-import Layout from "../Layout";
+import Layout from "./Layout";
 import SectionHeader from "@/Components/SectionHeader";
 import { FaGraduationCap, FaSearch, FaFilter } from "react-icons/fa";
 import HoverCard from "@/Components/HoverCard";
