cff2f239290e46841a0e9c12dc341fe5c8b69098
sokliNnuon1 <soklin061@gmail.com>
2025-08-12 17:31:36 +0700

    update home

---
+++ app/Models/Strategy.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
+
+class Strategy extends Model
+{
+    protected static $strategies = [
+        [
+            'id' => 1,
+            'title' => 'ការប្រឡង ថ្នាក់ទី១២ ឆ្នាំ២០២៣',
+            'image' => 'https://www.sparkadmissions.com/wp-content/uploads/2020/04/How_to_Get_Good_Grades_in_High_School.jpg',
+            'detail' => 'រយៈពេល៖ ២០០ នាទី',
+        ],
+        [
+            'id' => 2,
+            'title' => 'សៀវភៅបំពេញលំហាត់វិញ្ញាសា',
+            'image' => 'https://www.sparkadmissions.com/wp-content/uploads/2020/04/How_to_Get_Good_Grades_in_High_School.jpg',
+            'detail' => 'រយៈពេល៖ ២០០ នាទី',
+        ],
+        [
+            'id' => 3,
+            'title' => 'មេរៀនគន្លឹះជាប់លើកទី១',
+            'image' => 'https://www.rootsofaction.com/wp-content/uploads/2012/09/Good-grades-1.jpg',
+            'detail' => 'រយៈពេល៖ ១០០ នាទី',
+        ],
+        [
+            'id' => 4,
+            'title' => 'មេរៀនគន្លឹះជាប់លើកទី១',
+            'image' => 'https://myfirstnestegg.com/wp-content/uploads/student-proudly-holds-good-grade.png',
+            'detail' => 'រយៈពេល៖ ១០០ នាទី',
+        ],
+        [
+            'id' => 6,
+            'title' => 'ការប្រឡង ថ្នាក់ទី១២ ឆ្នាំ២០២៣',
+            'image' => 'https://www.sparkadmissions.com/wp-content/uploads/2020/04/How_to_Get_Good_Grades_in_High_School.jpg',
+            'detail' => 'រយៈពេល៖ ២០០ នាទី',
+        ],
+        
+    ];
+
+    public static function all($columns = ['*'])
+    {
+        return collect(self::$strategies);
+    }
+
+    public static function find($id)
+    {
+        return collect(self::$strategies)->firstWhere('id', $id);
+    }
+}
---
+++ app/Models/Video.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
+
+class Video extends Model
+{
+    protected static $videos = [
+        [
+            'id' => 1,
+            'title' => 'បទបង្ហាញ៖ ការរៀនដើម្បីទទួលបាននិទ្ទេស A',
+            'duration' => '15 នាទី',
+            'videoId' => 'bpFHTq529ME',
+            'thumbnail' => 'https://img.youtube.com/vi/bpFHTq529ME/hqdefault.jpg',
+            'videoUrl' => 'https://www.youtube.com/embed/bpFHTq529ME',
+        ],
+        [
+            'id' => 2,
+            'title' => 'បទបង្ហាញ៖ យុទ្ធសាស្រ្តសិក្សាថ្នាក់ទី១២',
+            'duration' => '20 នាទី',
+            'videoId' => 'dQw4w9WgXcQ',
+            'thumbnail' => 'https://img.youtube.com/vi/dQw4w9WgXcQ/hqdefault.jpg',
+            'videoUrl' => 'https://www.youtube.com/embed/dQw4w9WgXcQ',
+        ],
+        [
+            'id' => 3,
+            'title' => 'បទបង្ហាញ៖ របៀបរៀនអោយមានប្រសិទ្ធភាព',
+            'duration' => '18 នាទី',
+            'videoId' => 'tgbNymZ7vqY',
+            'thumbnail' => 'https://img.youtube.com/vi/tgbNymZ7vqY/hqdefault.jpg',
+            'videoUrl' => 'https://www.youtube.com/embed/tgbNymZ7vqY',
+        ],
+        [
+            'id' => 4,
+            'title' => 'បទបង្ហាញ៖ របៀបរៀនអោយមានប្រសិទ្ធភាព',
+            'duration' => '18 នាទី',
+            'videoId' => 'tgbNymZ7vqY',
+            'thumbnail' => 'https://img.youtube.com/vi/tgbNymZ7vqY/hqdefault.jpg',
+            'videoUrl' => 'https://www.youtube.com/embed/tgbNymZ7vqY',
+        ],
+    ];
+
+    public static function all($columns = ['*'])
+    {
+        return collect(self::$videos);
+    }
+
+    public static function find($id)
+    {
+        return collect(self::$videos)->firstWhere('id', $id);
+    }
+}
